897ea43391f50fd546c85fd7448f64880469328c
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-16 22:38:14 +0200

    created a css module

---
+++ modules.js
@@ -0,0 +1,59 @@
+const bookPreviewStyles = `
+  .book-preview {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    border: 1px solid #ddd;
+    margin-bottom: 10px;
+  }
+  .book-preview__image {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+  }
+  .book-preview__info {
+    display: flex;
+    flex-direction: column;
+  }
+  .book-preview__title {
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .book-preview__author {
+    font-size: 14px;
+    color: #555;
+  }
+`;
+
+const styles = `
+  .book-preview {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    border: 1px solid #ddd;
+    margin-bottom: 10px;
+  }
+  .book-preview__image {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+  }
+  .book-preview__info {
+    display: flex;
+    flex-direction: column;
+  }
+  .book-preview__title {
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .book-preview__author {
+    font-size: 14px;
+    color: #555;
+  }
+  .preview {
+    cursor: pointer;
+  }
+  .list__message_show {
+    display: block;
+  }
+`;
