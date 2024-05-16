8426cd44a66e0d7266eb841013eb360b5a301cd4
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-16 13:30:35 +0200

    commenting my code ..

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
+// Importing  data and utilities from other modules
 import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
 import { themeColor, remaining } from "./bookUtils.js";
 
+// Initializing variables
 let page = 1;
 let matches = books;
 
+// Function to create HTML elements
 function createBook(bookPreviews) {
-  console.log(bookPreviews);
+  // Extracting  information from book data
   const { id, image, title, author } = bookPreviews;
-
+ // Creating a button element 
   const element = document.createElement("button");
   element.classList = "preview";
   element.setAttribute("data-preview", id);
-
+ // Inner HTML with book information
   element.innerHTML = `
         <img
             class="preview__image"
@@ -26,49 +29,55 @@ function createBook(bookPreviews) {
 
   return element;
 }
-
+// Function to populate genre options in search 
 function genreOption() {
+  // Creating options for all genres
   const genreHtml = document.createDocumentFragment();
   const firstGenreElement = document.createElement("option");
   firstGenreElement.value = "any";
   firstGenreElement.innerText = "All Genres";
   genreHtml.appendChild(firstGenreElement);
-
+  // Populating genre options
   for (const [id, name] of Object.entries(genres)) {
     const element = document.createElement("option");
     element.value = id;
     element.innerText = name;
     genreHtml.appendChild(element);
   }
-
+  // Appending options to the search
   document.querySelector("[data-search-genres]").appendChild(genreHtml);
 }
-
+// Function to populate author options in search 
 function authorOption() {
+  // Creating options for all authors
   console.log(authorOption);
   const authorsHtml = document.createDocumentFragment();
   const firstAuthorElement = document.createElement("option");
   firstAuthorElement.value = "any";
   firstAuthorElement.innerText = "All Authors";
   authorsHtml.appendChild(firstAuthorElement);
-
+ // Populating author options from data
   for (const [id, name] of Object.entries(authors)) {
     const element = document.createElement("option");
     element.value = id;
     element.innerText = name;
     authorsHtml.appendChild(element);
   }
+  // Appending options to the search 
   document.querySelector("[data-search-authors]").appendChild(authorsHtml);
 }
-
+// Function to initialize theme based on user's system preferences
 function initializeTheme() {
+  // Checking if user prefers dark mode
   if (
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
   ) {
+     // Setting theme to night mode
     document.querySelector("[data-settings-theme]").value = "night";
     themeColor("night");
   } else {
+    // Setting theme to day mode
     document.querySelector("[data-settings-theme]").value = "day";
     themeColor("day");
   }
@@ -84,48 +93,50 @@ const overlayElement = dataSearchOverLay();
 function addEventListener(selector, eventType, eventHandler) {
   document.querySelector(selector).addEventListener(eventType, eventHandler);
 }
-
+// Function to add event listeners for various user interactions
 function initializeEventListener() {
+   // Event listener for canceling search overlay
   document
     .querySelector("[data-search-cancel]")
     .addEventListener("click", () => {
       overlayElement.open = false;
     });
-
+  // Event listener for canceling settings overlay
   addEventListener("[data-settings-cancel]", "click", () => {
     document.querySelector("[data-settings-overlay]").open = false;
   });
-
+  // Event listener for opening search overlay
   addEventListener("[data-header-search]", "click", () => {
     overlayElement.open = true;
     document.querySelector("[data-search-title]").focus();
   });
-
+  // Event listener for opening settings overlay
   addEventListener("[data-header-settings]", "click", () => {
     document.querySelector("[data-settings-overlay]").open = true;
   });
-
+  // Event listener for closing book preview overlay
   addEventListener("[data-list-close]", "click", () => {
     document.querySelector("[data-list-active]").open = false;
   });
-
+  // Event listener for submitting settings form
   addEventListener("[data-settings-form]", "submit", (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const { theme } = Object.fromEntries(formData);
-
+    // Changing theme based on user selection
     themeColor(theme);
-
+    // Closing settings overlay
     document.querySelector("[data-settings-overlay]").open = false;
   });
 }
+// Function to add event listener for submitting search 
 function addSearchFormSubmitListener() {
   addEventListener("[data-search-form]", "submit", (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const filters = Object.fromEntries(formData);
     const result = [];
-
+    // Filtering books based on search
     for (const book of books) {
       let genreMatch = filters.genre === "any";
 
@@ -145,7 +156,7 @@ function addSearchFormSubmitListener() {
         result.push(book);
       }
     }
-
+    // Updating displayed results
     page = 1;
     matches = result;
 
@@ -195,7 +206,7 @@ function addSearchFormSubmitListener() {
     overlayElement.open = false;
   });
 }
-
+// Function to handle click on pagination button
 function listButtonClickListener() {
   addEventListener("[data-list-button]", "click", () => {
     const fragment = document.createDocumentFragment();
@@ -228,11 +239,12 @@ function listButtonClickListener() {
     remaining(books, page, BOOKS_PER_PAGE);
   });
 }
+// Function to handle click on book previews
 function addListItemsClickListener() {
   addEventListener("[data-list-items]", "click", (event) => {
     const pathArray = Array.from(event.path || event.composedPath());
     let active = null;
-
+    // Finding the clicked book
     for (const node of pathArray) {
       if (active) break;
 
@@ -247,7 +259,7 @@ function addListItemsClickListener() {
         active = result;
       }
     }
-
+    // Displaying details of the clicked book
     if (active) {
       document.querySelector("[data-list-active]").open = true;
       document.querySelector("[data-list-blur]").src = active.image;
@@ -288,6 +300,7 @@ function settingsFormSubmit() {
     document.querySelector("[data-settings-overlay]").open = false;
   });
 }
+// Function to initialize app
 function initializeApp() {
   const starting = document.createDocumentFragment();
   for (const book of matches.slice(0, BOOKS_PER_PAGE)) {
@@ -295,7 +308,7 @@ function initializeApp() {
   }
 
   document.querySelector("[data-list-items]").appendChild(starting);
-
+  // Initializing
   genreOption();
   authorOption();
   initializeTheme();
@@ -309,14 +322,14 @@ function initializeApp() {
   ThemeFromStorage();
   addThemeToggle();
 }
-
+// Function to set theme based on local storage
 function ThemeFromStorage() {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     themeColor(savedTheme);
   }
 }
-
+// Function to handle theme change from settings
 function addThemeChange() {
   document
     .querySelector("[data-settings-theme]")
@@ -327,6 +340,7 @@ function addThemeChange() {
       saveThemePreference(theme);
     });
 }
+// Function to handle theme toggle
 function addThemeToggle() {
   // Add event listener for theme toggle button and save its state to local storage
   document
